40fdb93526bd0389b7f9cd2f8a9c8b65176eb299
kiokoshedy <shkmusembi@gmail.com>
2023-10-30 11:16:11 +0300

    changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
+  const [ setIndex] = useState(1);
   const toRotate = ["DevOps", "Backend Engineer", "Frontend Engineer"];
   const period = 2000;
 
