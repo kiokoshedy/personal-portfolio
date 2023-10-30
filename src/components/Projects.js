d6f0e871fccbcadc8aaeff4837b4e29ca5da6262
kiokoshedy <shkmusembi@gmail.com>
2023-10-30 17:04:32 +0300

    changes added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../utils/images/project-img11.png";
-import projImg2 from "../utils/images/project-img2.png";
+import projImg2 from "../utils/images/ecomerce.png";
 import projImg3 from "../utils/images/project-img3.png";
 import colorSharp2 from "../utils/images/color-sharp2.png";
 import "animate.css";
